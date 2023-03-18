6ac52df1f20a336555bd995a022d1fae91655092
Gonçalo Velosa <goncalo.velosa@yacoobalabs.com>
2023-03-18 14:37:23 +0000

    chore: add contract interface

---
+++ contract/contracts/interfaces/IILVIToken.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IILVIToken is IERC20 {
+    event BackupAddressSet(address indexed account, address indexed backupAddress);
+
+    event EmergencyTransfer(address indexed from, address indexed to, uint256 amount);
+
+    event BlacklistedAddressAdded(address indexed account);
+
+    function pause() external;
+
+    function unpause() external;
+
+    function mint(address to, uint256 amount) external;
+
+    function burn(address from, uint256 amount) external;
+
+    function domainSeparator() external view returns (bytes32);
+
+    function setBackupAddress(address backupAddress) external;
+
+    function emergencyTransfer(uint256 nonce, uint256 deadline, bytes calldata signature) external;
+
+    function blacklistAddress(address account) external;
+
+    function isBlacklisted(address account) external view returns (bool);
+
+    function getBackupAddress(address account) external view returns (address);
+}
