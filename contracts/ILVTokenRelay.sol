9452a37d41624ce47364920e7f881b934b82bacb
Gonçalo Velosa <goncalo.velosa@yacoobalabs.com>
2023-03-19 15:23:52 +0000

    feat: add relayer & update tests

---
+++ contracts/ILVTokenRelay.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "./interfaces/IILVTokenRelay.sol";
+
+import "./interfaces/IILVIToken.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ILVTokenRelay is IILVTokenRelay, Ownable {
+    IILVIToken public immutable ILVIToken;
+
+    constructor(address tokenAddress) {
+        ILVIToken = IILVIToken(tokenAddress);
+    }
+
+    /**
+     * @notice Emergency transfer of tokens from an address to it's backup address.
+     * @param owner token owner
+     * @param value amount of tokens to transfer
+     * @param deadline deadline for permit
+     * @param v signature part
+     * @param r signature part
+     * @param s signature part
+     */
+    function emergencyTransfer(address owner, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
+        uint256 balance = ILVIToken.balanceOf(owner);
+        require(balance > 0, "ILVTokenRelay: balance is zero");
+        ILVIToken.permit(owner, address(this), value, deadline, v, r, s);
+        address destinationAddress = ILVIToken.getBackupAddress(owner);
+        ILVIToken.transferFrom(owner, destinationAddress, value);
+    }
+}
---
+++ scripts/emergencyTransfer.ts
@@ -0,0 +1,38 @@
+import { BigNumber } from 'ethers'
+import { network } from 'hardhat'
+import { getPermitSignature } from '../test/fixtures'
+import { getRelayContract, getTokenContract } from './utils/getContract'
+import { getWallets } from './utils/signer'
+
+async function main() {
+  const ILVIToken = getTokenContract()
+  const ILVRelay = getRelayContract()
+
+  const accounts = await getWallets(network.name)
+  const account = accounts.noEthAccount
+  const backupAccount = await ILVIToken.getBackupAddress(account.address)
+
+  const balance = await ILVIToken.balanceOf(account.address)
+  const balanceParsed = balance.toBigInt()
+
+  console.log(
+    `Emergency transfer on network: ${network.name} from address ${account.address} to ${backupAccount} with amount ${balanceParsed}}`,
+  )
+
+  const deadLine = BigNumber.from(Math.floor(Date.now() / 1000) + 60 * 20)
+
+  const signature = await getPermitSignature(account, ILVIToken, ILVIToken.address, balanceParsed, deadLine)
+  const { v, r, s } = signature
+
+  const relayTx = await ILVRelay.connect(accounts.owner).emergencyTransfer(account.address, balance, deadLine, v, r, s)
+  const receipt = await relayTx.wait(2)
+
+  console.log(
+    `Emergency transfer from address ${account.address} to ${backupAccount} with amount ${balanceParsed} with tx hash ${receipt.transactionHash}`,
+  )
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
