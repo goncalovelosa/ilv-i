906e5004709d7fc9e6652a9c32570580bdba4434
Gonçalo Velosa <goncalo.velosa@yacoobalabs.com>
2023-03-19 15:22:01 +0000

    chore: move to base root since the small frontend app wont be submitted as of right now

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-contract-sizer'
+import 'dotenv/config'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-solhint'
+import '@nomiclabs/hardhat-ethers/signers'
+
+const alchemyKey = process.env.ALCHEMY_KEY
+const cmcKey = process.env.COINMARKETCAP_API_KEY
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  solidity: '0.8.18',
+  networks: {
+    ganache: {
+      url: 'http://localhost:8545',
+      gasPrice: 20000000000,
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${alchemyKey}`,
+      chainId: 5,
+    },
+  },
+  gasReporter: {
+    enabled: cmcKey ? true : false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    showTimeSpent: true,
+    currency: 'EUR',
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+    only: ['ILVIToken.sol'],
+  },
+}
+
+export default config
